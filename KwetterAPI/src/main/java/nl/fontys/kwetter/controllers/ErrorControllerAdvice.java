e7c54437c2a33ced573233948f60cb3f15113a22
Ronald van den Broek <Ronindebeek@users.noreply.github.com>
2019-02-23 18:13:16 +0100

    Added ErrorAdvice for the RestControllers

---
+++ KwetterAPI/src/main/java/nl/fontys/kwetter/controllers/ErrorControllerAdvice.java
@@ -0,0 +1,56 @@
+package nl.fontys.kwetter.controllers;
+
+import nl.fontys.kwetter.exceptions.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+@ControllerAdvice
+public class ErrorControllerAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(CannotLoginException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String cannotLoginException(CannotLoginException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(InvalidModelException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String invalidModelException(InvalidModelException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(KwetterDoesntExist.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String kwetterDoesntExist(KwetterDoesntExist ex) {
+        return ex.getMessage();
+    }
+
+
+    @ResponseBody
+    @ExceptionHandler(UserDoesntExist.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String userDoesntExist(UserDoesntExist ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(UsernameAlreadyExists.class)
+    @ResponseStatus(HttpStatus.GONE)
+    public String usernameAlreadyExists(UsernameAlreadyExists ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ExceptionHandler(NotImplementedException.class)
+    @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
+    public String notImplementedException(NotImplementedException ex) {
+        return ex.getMessage();
+    }
+}
--- KwetterAPI/src/main/java/nl/fontys/kwetter/controllers/LoginController.java
+++ KwetterAPI/src/main/java/nl/fontys/kwetter/controllers/LoginController.java
@@ -6,10 +6,7 @@ import nl.fontys.kwetter.models.Credentials;
 import nl.fontys.kwetter.models.User;
 import nl.fontys.kwetter.service.interfaces.ILoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
