8657bd64e08cf507b6142042755fc5d43e68dc7e
Ronald van den Broek <Ronindebeek@users.noreply.github.com>
2019-03-09 17:54:33 +0100

    Removing JUnit vintage requirement for tests

--- KwetterAPI/src/test/java/nl/fontys/kwetter/repository/DatabaseUserUnitTests.java
+++ KwetterAPI/src/test/java/nl/fontys/kwetter/repository/DatabaseUserUnitTests.java
@@ -1,19 +1,15 @@
 package nl.fontys.kwetter.repository;
 
-import nl.fontys.kwetter.Application;
 import nl.fontys.kwetter.models.Credentials;
 import nl.fontys.kwetter.models.User;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class DatabaseUserUnitTests {
 
