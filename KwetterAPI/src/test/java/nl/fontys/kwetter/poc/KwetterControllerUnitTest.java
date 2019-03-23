0eea257ccac4e84373c50623dd9f3952d185c8cd
Ronald van den Broek <Ronindebeek@users.noreply.github.com>
2019-03-24 00:03:49 +0100

    Removing junit 4 dependancy

--- KwetterAPI/src/test/java/nl/fontys/kwetter/poc/KwetterControllerUnitTest.java
+++ KwetterAPI/src/test/java/nl/fontys/kwetter/poc/KwetterControllerUnitTest.java
@@ -1,49 +1,36 @@
 package nl.fontys.kwetter.poc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import nl.fontys.kwetter.controllers.KwetterController;
 import nl.fontys.kwetter.models.Kwetter;
 import nl.fontys.kwetter.models.User;
-import nl.fontys.kwetter.repository.ICredentialsRepository;
-import nl.fontys.kwetter.repository.IKwetterRepository;
-import nl.fontys.kwetter.repository.IUserRepository;
-import nl.fontys.kwetter.service.implementation.KwetterService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import nl.fontys.kwetter.service.IKwetterService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Calendar;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(KwetterController.class)
-public class KwetterControllerUnitTest {
+@SpringBootTest
+@AutoConfigureMockMvc(secure = false)
+class KwetterControllerUnitTest {
 
     @Autowired
     private MockMvc mvc;
 
     @MockBean
-    private KwetterService kwetterService;
-
-    @MockBean
-    private IKwetterRepository kwetterRepository;
-
-    @MockBean
-    private IUserRepository userRepository;
-
-    @MockBean
-    private ICredentialsRepository credentialsRepository;
+    private IKwetterService kwetterService;
 
     @Test
-    public void createKwetter() {
+    void createKwetter() {
         ObjectMapper mapper = new ObjectMapper();
 
         User user = new User();
@@ -51,7 +38,7 @@ public class KwetterControllerUnitTest {
 
         Kwetter kwetter = new Kwetter("Test", user, Calendar.getInstance().getTime());
         try {
-            when(kwetterService.createKwetter(user.getId(), kwetter)).thenReturn(kwetter);
+            Mockito.when(kwetterService.createKwetter(user.getId(), kwetter)).thenReturn(kwetter);
 
             mvc.perform(post("/kwetter/create/" + user.getId())
                     .contentType(MediaType.APPLICATION_JSON)
@@ -60,6 +47,7 @@ public class KwetterControllerUnitTest {
                     .andExpect(status().isOk());
         } catch (Exception e) {
             e.printStackTrace();
+            fail("");
         }
     }
 }
