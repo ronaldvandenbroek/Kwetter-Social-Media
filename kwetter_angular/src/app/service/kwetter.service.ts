5388ef6369021047343c3e0baaf747695d7176fc
Ronald van den Broek <ronaldvandenbroek@users.noreply.github.com>
2019-05-26 22:21:34 +0200

    Ts lint fix

--- kwetter_angular/src/app/service/kwetter.service.ts
+++ kwetter_angular/src/app/service/kwetter.service.ts
@@ -10,19 +10,27 @@ export class KwetterService {
   }
 
   public timeline(): Observable<Kwetter[]> {
-    return this.http.get<Kwetter[]>(this.authenticationService.getHref('timeline'), {headers: this.authenticationService.authenticationHeaders});
+    return this.http.get<Kwetter[]>(
+      this.authenticationService.getHref('timeline'),
+      {headers: this.authenticationService.authenticationHeaders});
   }
 
   createKwetter(text: string) {
     const kwetter = new Kwetter();
     kwetter.text = text;
-    const response = this.http.post<Kwetter>(this.authenticationService.getHref('create'), kwetter, {headers: this.authenticationService.authenticationHeaders});
+    const response = this.http.post<Kwetter>(
+      this.authenticationService.getHref('create'),
+      kwetter,
+      {headers: this.authenticationService.authenticationHeaders});
     response.subscribe(() => {
     });
     return response;
   }
 
   searchKwetter(text: string): Observable<Kwetter[]> {
-    return this.http.post<Kwetter[]>(this.authenticationService.getHref('search'), text, {headers: this.authenticationService.authenticationHeaders});
+    return this.http.post<Kwetter[]>(
+      this.authenticationService.getHref('search'),
+      text,
+      {headers: this.authenticationService.authenticationHeaders});
   }
 }
