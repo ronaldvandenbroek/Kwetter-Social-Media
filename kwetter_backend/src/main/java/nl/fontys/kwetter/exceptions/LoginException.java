fd675da984d6a3ecce9854ad4a4b81ddb264ed3e
Ronald van den Broek <ronaldvandenbroek@users.noreply.github.com>
2019-05-27 13:14:48 +0200

    Fixing bug with errors not showing up in the frontend

--- kwetter_backend/src/main/java/nl/fontys/kwetter/exceptions/LoginException.java
+++ kwetter_backend/src/main/java/nl/fontys/kwetter/exceptions/LoginException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
-public class LoginException extends Exception {
+public class LoginException extends RuntimeException  {
 
     public LoginException(String message) {
         super(message);
--- kwetter_backend/src/main/java/nl/fontys/kwetter/exceptions/ModelNotFoundException.java
+++ kwetter_backend/src/main/java/nl/fontys/kwetter/exceptions/ModelNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ModelNotFoundException extends Exception {
+public class ModelNotFoundException extends RuntimeException  {
 
     public ModelNotFoundException(String message) {
         super(message);
